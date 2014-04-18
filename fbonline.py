cbd2713969f89db2ff13847ed4f96b4f7c80ba8c
Malith Senaweera <malithsenaweera@gmail.com>
2014-04-18 12:42:07 +0530

    Notify when a given user comes online

--- fbonline.py
+++ fbonline.py
@@ -2,10 +2,15 @@ import requests
 import json
 import subprocess
 import time
-ACCESS_TOKEN = "CAADbJ2R6rpEBAKUP0NUVO5TrSQWk6B1pth9wxRQRZArTcvYvTAFHvw1AFh02qSAN8HZBGtVauDvQZAg61BpGzC3zcvYAkZAxOlyKsY4evM8t5LQ0ZAoYMfJpDSLMZBy4Qig9cP5JYeqQeLvMnhZAzgW4cccnZBr84KPMjrvaBOgPO90Gs0SkNYg1EIMdXnQDnQoZD"
+import argparse
+ACCESS_TOKEN = "ENTER YOUR ACCESS TOKEN"
 friends_online = set()
-friends_online_old = set() 
+friends_online_old = set()
 counter = 0
+
+parser = argparse.ArgumentParser(description="fbnotify")
+parser.add_argument('name', nargs='?', help='Name of the user')
+args = parser.parse_args()
 
 def get_friends_online():
     '''Returns friends who are online'''
@@ -15,17 +20,37 @@ def get_friends_online():
     r = requests.get('https://graph.facebook.com/fql', params=payload)
     result = json.loads(r.text)
     for name in result['data']:
-	    friends_online.add(name['name'])
+        friends_online.add(name['name'])
     return friends_online
 
-while 1:
-    if counter % 30 == 0:
-        friends_online_old = []
-    if len(get_friends_online()) > 0:
-        for friend in friends_online:
-	        if not friend in friends_online_old:
-	            message = "%s is online" % friend
-	            subprocess.Popen(['notify-send', message])
-	            friends_online_old.append(friend)
-    time.sleep(60)
-    counter += 1
+def monitorAll():
+    global friends_online
+    global friends_online_old
+    global counter
+    while 1:
+        if counter % 30 == 0:
+            friends_online_old = []
+        if len(get_friends_online()) > 0:
+            for friend in friends_online:
+                if not friend in friends_online_old:
+                    message = "%s is online" % friend
+                    subprocess.Popen(['notify-send', message])
+                    friends_online_old.append(friend)
+        time.sleep(60)
+        counter += 1
+
+def stalk(victim):
+    while 1:
+        friends_online = get_friends_online()
+        friends_online = map(lambda x: x.split(" "), friends_online)
+        sublists = [victim in i for i in friends_online]
+        if True in sublists:
+            fullName = ' '.join(friends_online[sublists.index(True)])
+            message = "Psst! %s is online" % fullName
+            subprocess.Popen(['notify-send', message])
+        time.sleep(60)
+
+if args.name:
+    stalk(args.name)
+else:
+    monitorAll()
